cb9ae9093e1ef84e040142d8c8ca0268185e8605
nadaalynasr <nadaalynasr@gmail.com>
2025-11-28 00:25:43 -0500

    map functional

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import '@/styles/tailwind.css';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7420123397062174"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
+      <body>{children}</body>
+    </html>
+  )
+}
