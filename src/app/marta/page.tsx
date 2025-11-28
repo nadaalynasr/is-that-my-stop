f1fcc9657c422936b57e2dc67c5b3ad817be88f6
nadaalynasr <nadaalynasr@gmail.com>
2025-11-27 21:15:24 -0500

    file structure

---
+++ src/app/marta/page.tsx
@@ -0,0 +1,10 @@
+import data from './data/features.json'
+
+
+export default function MARTA() {
+    return (
+
+    )
+
+}
+
---
+++ src/types/index.ts
@@ -0,0 +1,58 @@
+import { Feature, Point, LineString, FeatureCollection } from 'geojson';
+
+export interface StationProperties {
+  id: number;
+  name: string;
+  alternate_names?: string[];
+  line: string;
+  order: number;
+}
+
+export interface StationFeature extends Feature<Point, StationProperties> {
+  id: number;
+}
+
+export interface RouteProperties {
+  line: string;
+  color: string;
+}
+
+export interface RouteFeature extends Feature<LineString, RouteProperties> {}
+
+export interface Line {
+  id: string;
+  name: string;
+  color: string;
+  order: number;
+}
+
+export interface Lines {
+  [lineId: string]: Line;
+}
+
+export interface Guess {
+  station: StationFeature;
+  distance: number;
+  proximity: ProximityLevel;
+}
+
+export interface ProximityLevel {
+  level: 'correct' | 'hot' | 'warm' | 'cold' | 'freezing';
+  color: string;
+  text: string;
+}
+
+export interface GameState {
+  targetStation: StationFeature | null;
+  guesses: Guess[];
+  gameWon: boolean;
+  guessedStationIds: Set<number>;
+}
+
+export interface CityConfig {
+  name: string;
+  slug: string;
+  center: [number, number];
+  zoom: number;
+  bounds: [[number, number], [number, number]];
+}
